efc5d48b122a084fc8d4833e9f424258958558f4
abdessamadsalim <samimalim1991@gmail.com>
2024-05-12 05:05:15 -0700

    upd

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,7 +1,6 @@
-# app.py
 #!/usr/bin/python3
 """
-Web server
+Web server 
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
@@ -9,10 +8,13 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ json 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # python -m api.v1.app 
+    app.run(host="0.0.0.0", port=5000)
