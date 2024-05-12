bfad9baa569a81b5fa4d2b15f4b7422c7e6f884e
abdessamadsalim <samimalim1991@gmail.com>
2024-05-12 04:21:07 -0700

    upd squa

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
     width = 0
     height = 0
@@ -12,17 +12,19 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
+
